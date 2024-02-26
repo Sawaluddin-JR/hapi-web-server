e7582c17373868ee4c2aef34895cc09db43ab3ad
Sawaluddin <sawaluddinsrg472@gmail.com>
2024-02-26 13:29:40 +0700

    Response Toolkit

--- routes.js
+++ routes.js
@@ -50,6 +50,24 @@ const routes = [
     },
   },
   {
+    method: "POST",
+    path: "/user",
+    handler: (request, h) => {
+      //return h.response("created").code(201);
+
+      // Detail notation
+      // const response = h.response("success");
+      // response.type("text/plain");
+      // response.header("Custom-Header","some-value");
+      // return response;
+
+      //Chained notation
+      return h.response("success")
+      .type("text/plain")
+      .header("Custom-Header","some-value");
+    },
+  },
+  {
     method: "*",
     path: "/{any*}",
     handler: (request, h) => {
